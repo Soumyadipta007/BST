e623ec9748e431acaeb50de2b2fa09ad3bff2c3d
Soumya <soumyadipta0077@gmail.com>
2020-11-01 20:13:55 +0530

    [Soumya] Add: Creating BST

---
+++ BST_DS/BST_DS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BST_DS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            Console.WriteLine("Binary Search Tree -");
+            binarySearchTree.Display();
+        }
+    }
+}
